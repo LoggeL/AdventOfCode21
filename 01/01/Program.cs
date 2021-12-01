d30721e4e2c86cd70d153d18f4d19c2465396c94
LoggeL <hyper.xjo@gmx.de>
2021-12-01 14:29:38 +0100

    Day 1

---
+++ 01/01/Program.cs
@@ -0,0 +1,17 @@
+﻿int increase = 0;
+int previous = 0;
+int rollingAverage = 1;
+
+string[] lines = System.IO.File.ReadAllLines(@"Input.txt");
+for (int i = 0; i <= lines.Length - rollingAverage; i++) {
+    int value = 0;
+    for (int j = 0; j < rollingAverage; j++) {
+        value += int.Parse(lines[i + j]);
+    }
+    if (previous != 0 && value > previous) {
+        increase++;
+    }
+    previous = value;
+}
+
+Console.WriteLine($"Number of decreases is {increase}!");
